f334132dc618eb0575d4819b6cd58ac8740958d1
Aaron Suggs <aaron@ktheory.com>
2015-01-27 21:32:27 -0500

    Add some tests

--- Rakefile
+++ Rakefile
@@ -3,17 +3,7 @@ require "bundler/setup"
 require "bundler/gem_tasks"
 require 'rake/testtask'
 
-namespace :test do
-  Rake::TestTask.new(:units) do |t|
-    t.pattern = "spec/*_spec.rb"
-  end
-
-  Rake::TestTask.new(:integration) do |t|
-    t.pattern = "spec/integration/*_spec.rb"
-  end
+Rake::TestTask.new do |t|
+  t.pattern = "spec/*_spec.rb"
 end
-
-desc 'Run tests'
-task :test => %w[test:units test:integration]
-
 task :default => :test
---
+++ spec/cfn-flow_template_spec.rb
@@ -0,0 +1,56 @@
+require_relative 'spec_helper'
+
+describe 'CfnFlow::Template' do
+
+  let(:template) {
+    CfnFlow::Template.new(from: 'spec/data/sqs.template', prefix: 'p', bucket: 'b')
+  }
+
+  let(:yml_template) {
+    CfnFlow::Template.new(from: 'spec/data/sqs.yml', prefix: 'p', bucket: 'b')
+  }
+
+  let(:not_a_template) {
+    CfnFlow::Template.new(from: 'spec/data/cfn-flow.yml', prefix: 'p', bucket: 'b')
+  }
+
+  describe '#initialize' do
+    subject { CfnFlow::Template }
+
+    it('succeeds') do
+      subject.new(from: 'f', prefix: 'p', bucket: 'b').must_be_kind_of CfnFlow::Template
+    end
+
+    it('requires args') do
+      -> { subject.new }.must_raise(ArgumentError)
+    end
+  end
+
+  describe '#yaml?' do
+    it 'works' do
+      yml_template.yaml?.must_equal true
+      yml_template.json?.must_equal false
+      template.yaml?.must_equal false
+      template.json?.must_equal true
+    end
+  end
+
+  describe '#is_cfn_template?' do
+    it 'works' do
+      yml_template.is_cfn_template?.must_equal true
+      template.is_cfn_template?.must_equal true
+      not_a_template.is_cfn_template?.must_equal false
+    end
+  end
+
+  describe '#validate!' do
+    it 'can succeed' do
+      template.send(:cfn).stub_responses(:validate_template)
+      template.validate!
+    end
+    it 'can raise an error' do
+      template.send(:cfn).stub_responses(:validate_template, 'ValidationError')
+      -> { template.validate! }.must_raise Aws::CloudFormation::Errors::ValidationError
+    end
+  end
+end
