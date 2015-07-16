83d251b118682d6364425bea39f505afd2441fb8
Aaron Suggs <aaron@ktheory.com>
2015-07-16 18:22:05 -0400

    Refactor validate command

--- lib/cfn-flow/cli.rb
+++ lib/cfn-flow/cli.rb
@@ -1,117 +1,139 @@
-class CfnFlow::CLI < Thor
+module CfnFlow
+  class CLI < Thor
 
-  def self.shared_options
-    method_option :bucket,     type: :string, desc: 'S3 bucket for templates'
-    method_option :to,         type: :string, desc: 'S3 path prefix for templates'
-    method_option :from,       type: :string, desc: 'Local source directory for templates'
-    method_option 'dev-name',  type: :string, desc: 'Personal development prefix'
-    method_option :region,     type: :string, desc: 'AWS Region'
-    method_option :verbose,    type: :boolean, desc: 'Verbose output', default: false
-  end
+    def self.exit_on_failure?
+      CfnFlow.exit_on_failure?
+    end
 
-  def self.exit_on_failure?
-    CfnFlow.exit_on_failure?
-  end
+    no_commands do
+      def load_config
+        defaults = { 'from' => '.' }
+        file_config = begin
+                        YAML.load_file(ENV['CFN_FLOW_CONFIG'] || './cfn-flow.yml')
+                      rescue Errno::ENOENT
+                        {}
+                      end
+        env_config = {
+          'bucket'      => ENV['CFN_FLOW_BUCKET'],
+          'to'          => ENV['CFN_FLOW_TO'],
+          'from'        => ENV['CFN_FLOW_FROM'],
+          'dev-name'    => ENV['CFN_FLOW_DEV_NAME'],
+          'region'      => ENV['AWS_REGION']
+        }.delete_if {|_,v| v.nil?}
 
-  no_commands do
-    def load_config
-      defaults = { 'from' => '.' }
-      file_config = begin
-                      YAML.load_file(ENV['CFN_FLOW_CONFIG'] || './cfn-flow.yml')
-                    rescue Errno::ENOENT
-                      {}
-                    end
-      env_config = {
-        'bucket'      => ENV['CFN_FLOW_BUCKET'],
-        'to'          => ENV['CFN_FLOW_TO'],
-        'from'        => ENV['CFN_FLOW_FROM'],
-        'dev-name'    => ENV['CFN_FLOW_DEV_NAME'],
-        'region'      => ENV['AWS_REGION']
-      }.delete_if {|_,v| v.nil?}
+        # Env vars override config file. Command args override env vars.
+        self.options = defaults.merge(file_config).merge(env_config).merge(options)
 
-      # Env vars override config file. Command args override env vars.
-      self.options = defaults.merge(file_config).merge(env_config).merge(options)
+        # Ensure region env var is set for AWS client
+        ENV['AWS_REGION'] = options['region']
 
-      # Ensure region env var is set for AWS client
-      ENV['AWS_REGION'] = options['region']
+        # validate required options are present
+        %w(region bucket to from).each do |arg|
+          unless options[arg]
+            raise Thor::RequiredArgumentMissingError.new("Missing required argument '#{arg}'")
+          end
+        end
 
-      # validate required options are present
-      %w(region bucket to from).each do |arg|
-        unless options[arg]
-          raise Thor::RequiredArgumentMissingError.new("Missing required argument '#{arg}'")
+        unless options['dev-name'] || options['release']
+          raise Thor::RequiredArgumentMissingError.new("Missing either 'dev-name' or 'release' argument")
         end
       end
 
-      unless options['dev-name'] || options['release']
-        raise Thor::RequiredArgumentMissingError.new("Missing either 'dev-name' or 'release' argument")
+    end
+
+    ##
+    # Template methods
+
+    desc 'validate TEMPLATE [...]', 'Validates templates'
+    method_option :verbose, type: :boolean, desc: 'Verbose output', default: false
+    def validate(*templates)
+
+      if templates.empty?
+        raise Thor::RequiredArgumentMissingError.new('You must specify a template to validate')
       end
-    end
 
-    shared_options
-    def load_templates
-      load_config
-      glob = File.join(options['from'], '**/*.{yml,json,template}')
-
-      puts Dir.glob(glob.inspect)
-      @templates = Dir.glob(glob).map { |path|
-        CfnFlow::Template.new(from: path, bucket: options['bucket'], prefix: prefix)
-      }.select {|t|
-        verbose "Checking file #{t.from}... "
-        if t.is_cfn_template?
-          verbose "loaded"
-          true
-        else
-          verbose "skipped."
-          false
-        end
-      }
-    end
-  end
-
-  desc :validate, 'Validates templates'
-  shared_options
-  def validate
-    load_templates
-    @templates.each do |t|
-      begin
-        say "Validating #{t.from}... "
-        t.validate!
-        say "valid."
-      rescue Aws::CloudFormation::Errors::ValidationError
-        raise Thor::Error.new("Error validating #{t.from}. Message: #{$!.message}")
+      templates.map{|path| Template.new(path) }.each do |template|
+        say "Validating #{template.local_path}... "
+        template.validate!
+        say 'valid.', :green
       end
-    end
-  end
-
-  desc :upload, 'Validate & upload templates to the CFN_FLOW_DEV_NAME prefix'
-  shared_options
-  method_option :release, type: :string, lazy_default: CfnFlow::Git.sha, desc: 'Upload release'
-  def upload
-    CfnFlow::Git.check_status if options['release']
-
-    validate
-    @templates.each do |t|
-      say "Uploading #{t.from} to #{t.url}"
-      t.upload!
+    rescue Aws::CloudFormation::Errors::ValidationError => e
+      raise Thor::Error.new("Invalid template. Message: #{e.message}")
+    rescue CfnFlow::Template::Error => e
+      raise Thor::Error.new("Error loading template. (#{e.class}) Message: #{e.message}")
     end
 
-  end
-  default_task :upload
+    desc 'publish TEMPLATE [...]', 'Validate & upload templates to the CFN_FLOW_DEV_NAME prefix'
+    method_option 'dev-name', type: :string, desc: 'Personal development prefix'
+    method_option :release,   type: :string, desc: 'Upload release', lazy_default: CfnFlow::Git.sha
+    method_option :verbose,   type: :boolean, desc: 'Verbose output', default: false
+    def publish(*templates)
+      CfnFlow::Git.check_status if options['release']
 
-  private
-  def verbose(msg)
-    say msg if options['verbose']
-  end
+      validate
+      @templates.each do |t|
+        say "Uploading #{t.from} to #{t.url}"
+        t.upload!
+      end
 
-  def prefix
-    # Add the release or dev name to the prefix
-    parts = []
-    parts << options['to']
-    if options['release']
-      parts += [ 'release',  options['release'] ]
-    else
-      parts += [ 'dev', options['dev-name'] ]
     end
-    File.join(*parts)
+
+    ##
+    # Stack methods
+
+    desc 'deploy ENVIRONMENT', 'Launch a stack'
+    def deploy(environment)
+      # TODO
+      # Optionally invoke cleanup
+    end
+
+    desc :list, 'List running stacks'
+    def list
+      # TODO
+    end
+
+    desc 'show STACK', 'Show details about STACK'
+    def show(stack)
+      # TODO
+    end
+
+    desc 'events STACK', 'List events for  STACK'
+    def events(stack)
+      # TODO
+    end
+
+
+    desc 'cleanup', 'Shut down a stack'
+    method_option :force, type: :boolean, default: false, desc: 'Shut down without confirmation'
+    method_option :except, type: :string, desc: 'A list of stacks to omit from cleanup'
+    def cleanup
+      # TODO
+    end
+
+
+
+
+
+
+
+
+
+
+    private
+    def verbose(msg)
+      say msg if options['verbose']
+    end
+
+    def prefix
+      # Add the release or dev name to the prefix
+      parts = []
+      parts << options['to']
+      if options['release']
+        parts += [ 'release',  options['release'] ]
+      else
+        parts += [ 'dev', options['dev-name'] ]
+      end
+      File.join(*parts)
+    end
   end
 end
--- spec/cfn-flow/cli_spec.rb
+++ spec/cfn-flow/cli_spec.rb
@@ -2,6 +2,7 @@ require_relative '../helper'
 
 describe 'CfnFlow::CLI' do
   let(:cli) { CfnFlow::CLI }
+  let(:template) { 'spec/data/sqs.yml' }
 
   before do
     ENV.update({
@@ -12,15 +13,34 @@ describe 'CfnFlow::CLI' do
   end
 
   describe '#validate' do
-    it('succeeds') {
-      capture { cli.start %w[validate] }
-    }
+    it 'succeeds' do
+      out, err = capture_io { cli.start [:validate, template] }
+      err.must_be :empty?
+      out.must_match "Validating #{template}... valid."
+    end
 
-    it('can fail') {
+    it 'can have multiple templates' do
+      out, _ = capture_io { cli.start [:validate, template, 'spec/data/sqs.template'] }
+      out.split("\n").size.must_equal 2
+    end
+
+    it 'can fail with malformed templates' do
+      _, err = capture_io { cli.start [:validate, 'no/such/template'] }
+      err.must_match 'Error loading template'
+      err.must_match 'Errno::ENOENT'
+    end
+
+    it 'can fail with validation error' do
       Aws.config[:cloudformation] = {stub_responses: {validate_template: 'ValidationError'}}
-      _, err = capture_io { cli.start %w(validate) }
-      err.must_match "Error validating"
-    }
+      _, err = capture_io { cli.start [:validate, template] }
+      err.must_match "Invalid template"
+    end
+
+    it 'fails when no templates are passed' do
+      out, err = capture_io { cli.start [:validate] }
+      out.must_be :empty?
+      err.must_match 'You must specify a template to validate'
+    end
 
   end
 
