534df05ba0429a822d4ca68c58859d4d203ab5d8
Aaron Suggs <aaron@ktheory.com>
2015-07-20 14:50:47 -0400

    Add EventPresenter class

--- spec/helper.rb
+++ spec/helper.rb
@@ -17,6 +17,7 @@ ENV['AWS_ACCESS_KEY_ID'] = 'test-key'
 ENV['AWS_SECRET_ACCESS_KEY'] = 'test-secret'
 ENV['CFN_FLOW_DEV_NAME'] = 'cfn-flow-specs'
 ENV['CFN_FLOW_CONFIG_PATH'] = 'spec/data/cfn-flow.yml'
+ENV['CFN_FLOW_EVENT_POLLING_INTERVAL'] = '0'
 
 class Minitest::Spec
   # From http://git.io/bcfh
@@ -49,6 +50,36 @@ class Minitest::Spec
     # Reset stubs
     CfnFlow.clear!
     Aws.config.delete(:cloudformation)
+  end
+
+  def stub_stack_data(attrs = {})
+    {
+      stack_name: "mystack",
+      stack_status: 'CREATE_COMPLETE',
+      creation_time: Time.now,
+      tags: [
+        {key: 'CfnFlowService', value: CfnFlow.service},
+        {key: 'CfnFlowEnvironment', value: 'production'}
+      ]
+    }.merge(attrs)
+  end
+
+  def stub_event_data(attrs = {})
+    {
+      stack_id: 'mystack',
+      stack_name: 'mystack',
+      event_id: SecureRandom.hex,
+      resource_status: 'CREATE_COMPLETE',
+      logical_resource_id: 'stubbed-resource-id',
+      resource_type: 'stubbed-resource-type',
+      timestamp: Time.now
+    }.merge(attrs)
+  end
+
+  def stub_event(attrs = {})
+    data = stub_event_data(attrs)
+    id = data.delete(:event_id)
+    Aws::CloudFormation::Event.new(id: id, data: data)
   end
 
 end
